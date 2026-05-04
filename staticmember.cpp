e039ccb7b4ce5059271f25a94c8d38fbbc64a6cb
nanayyayaa <jiejueejuju@gmail.com>
2026-05-04 18:13:31 +0700

    first commit

---
+++ staticmember.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa { //membuat class mahasiswa
+
+public:
+    static int nim; //deklarasi variabel static yaitu variabel nim
+    //deklarasi variabel member
+    int id;
+    string nama;
+
+    //deklarasi prosedur setID() dan printAll()
+    void setID();
+    void printAll();
+};
