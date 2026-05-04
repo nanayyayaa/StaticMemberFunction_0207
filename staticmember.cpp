0fb149ce25a568a2596749e08f5a2439ef891ba6
nanayyayaa <jiejueejuju@gmail.com>
2026-05-04 18:20:21 +0700

    commit3

--- staticmember.cpp
+++ staticmember.cpp
@@ -28,4 +28,18 @@ void Mahasiswa::setID() {
 };
 
 void Mahasiswa::printAll(){
+
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+};
+
+int main(){
+    //membuat object dan memberi nilai
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    //memanggil prosedur printAll()
 }
