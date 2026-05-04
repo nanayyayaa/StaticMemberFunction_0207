1bedc12eee6deee2d4079770a8c415ba94386957
nanayyayaa <jiejueejuju@gmail.com>
2026-05-04 19:15:30 +0700

    commit 14

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -20,3 +20,31 @@ mahasiswa :: mahasiswa(){
 mahasiswa :: mahasiswa(int iNim){
     nim = iNama; // definisi hanya NIM
 }
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; // definisi hanya NAMA
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim; // definisi NIM dan NAMA
+    nama = iNama;
+}
+
+void mahasiswa::cetak(){
+
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main() {
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("Andi");
+    mahasiswa mhs3(01023, "Santi");
+
+    // tampilkan nilai
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
