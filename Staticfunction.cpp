a86967b64964b472b9b32d85aed3ef1507339eb4
nanayyayaa <jiejueejuju@gmail.com>
2026-05-04 18:39:30 +0700

    commit 9

--- Staticfunction.cpp
+++ Staticfunction.cpp
@@ -56,4 +56,9 @@ int main () {
     mhs4.printAll();
 
     //menampilkan NIM terakhir yang diproses
+    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
+    system("pause");
+
+    return 0;
+    
 }
